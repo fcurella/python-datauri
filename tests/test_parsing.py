b853741a7f65d7a42649e5d249635290300e6260
Flavio Curella <flavio.curella@gmail.com>
2017-06-16 12:50:05 -0500

    add some tests

---
+++ tests/test_parsing.py
@@ -0,0 +1,18 @@
+import unittest
+
+from datauri import DataURI
+
+
+class ParseTestCase(unittest.TestCase):
+
+    def test_parse(self):
+        t = 'data:text/plain;charset=utf-8;base64,VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wZWQgb3ZlciB0aGUgbGF6eSBkb2cu'
+        DataURI(t)
+
+    def test_parse_name(self):
+        t = 'data:text/plain;name=file-1_final.txt;charset=utf-8;base64,VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wZWQgb3ZlciB0aGUgbGF6eSBkb2cu'
+        DataURI(t)
+
+    def test_parse_name_no_charset(self):
+        t = 'data:text/plain;name=file.txt;base64,VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wZWQgb3ZlciB0aGUgbGF6eSBkb2cu'
+        DataURI(t)
